2df6cad12cc6a7237c1f2be3a613727916c00e60
Your Name <1821503@iub.edu.bd>
2025-04-11 18:54:48 +0600

    All

---
+++ src/pages/api/feedback.js
@@ -0,0 +1,30 @@
+
+import fs from 'fs';
+import path from 'path';
+
+
+function handler(req,res){
+    if(req.method==='POST'){
+        const email=req.body.email;
+        const feedbackText=req.body.text;
+
+        const newFeedback={
+            id:new Date().toISOString(),
+            email:email,
+            tet:feedbackText
+        }
+        const filePath = path.json(process.cwd(),'data','feedback.json');
+        const fileData = fs.readFileSync(filePath)
+        const data=JSON.parse(fileData)
+        data.push(newFeedback)
+        fs.writeFileSync(filePath,JSON.stringify(data))
+        res.status(201).json({message:'success',feedack:newFeedback})
+
+    }else{
+        res.status(200).json({message:'This works!'});
+    }
+   
+
+
+}
+export default handler;
